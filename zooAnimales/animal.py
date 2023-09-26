3ebf916123b9e426aee211975f4e5ca8684c7452
ronalcastro97 <105658996+ronalcastro97@users.noreply.github.com>
2023-09-26 00:53:00 -0500

    Update animal.py

--- zooAnimales/animal.py
+++ zooAnimales/animal.py
@@ -3,7 +3,7 @@ import zooAnimales
 
 class Animal():
     _totalAnimales=0
-    _zona= ""
+    _zona= None
     def __init__(self,nombre,edad,habitat,genero):
         self._nombre=nombre
         self._edad=edad
@@ -57,7 +57,7 @@ class Animal():
         return "Mamiferos : " + str(zooAnimales.mamifero.Mamifero.cantidadMamiferos()) + "\nAves : " + str(zooAnimales.ave.Ave.cantidadAves()) + "\nReptiles : " + str(zooAnimales.reptil.Reptil.cantidadReptiles()) + "\nPeces : " + str(zooAnimales.pez.Pez.cantidadPeces()) + "\nAnfibios : " + str(zooAnimales.anfibio.Anfibio.cantidadAnfibios())
 
     def toString(self):
-        if self._zona != "":
+        if self._zona != None:
             return "Mi nombre es " + self._nombre + ", tengo una edad de " + str(self._edad) + ", habito en " + self._habitat + " y mi genero es " + self._genero + ", la zona en la que me ubico es " + self._zona.getNombre() + ", en el zoo " + self._zona.getZoo().getNombre()
         else:
             return "Mi nombre es " + self._nombre + ", tengo una edad de " + str(self._edad) + ", habito en " + self._habitat + " y mi genero es " + self._genero
